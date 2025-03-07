a29debe61a079f06c17213745bc17961be704037
Rahul2006019 <vigneshrahulr@gmail.com>
2025-03-07 12:02:49 +0530

    updated

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,11 @@
+function Greeting() {
+    return (
+      <div>
+        <h2>Welcome to React, Kalvian! 🚀</h2>
+        <p>React components help in reusability! 💡</p>
+      </div>
+    );
+  }
+  
+  export default Greeting;
+  
